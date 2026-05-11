ae1d78946d5aba4ad8fa85ce6cbc599ef4358238
natigoat37 <natigoat300@gmail.com>
2026-05-11 16:26:55 +0300

    feat: implement operate function to route math operations

--- index.js
+++ index.js
@@ -13,4 +13,13 @@ function divide(a, b) {
     return a / b;
 }
 
+let num1 = Number("1");
+let operator = "";
+let num2 = Number("4");
 
+function operate() {
+    if (operator == "+") return add(num1, num2);
+    else if (operator == "-") return subtract(num1, num2);
+    else if (operator == "*") return multiply(num1, num2);
+    else if ( operator == "÷") return divide(num1, num2);
+}
