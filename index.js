11bc3a812c01cb647c2dbaadd5ee6a7a9554e7b2
natigoat37 <natigoat300@gmail.com>
2026-05-11 19:43:56 +0300

    feat: implement digit input and calculation on equals click

--- index.js
+++ index.js
@@ -13,13 +13,58 @@ function divide(a, b) {
     return a / b;
 }
 
-let num1 = Number("1");
+let num1 = "";
 let operator = "";
-let num2 = Number("4");
+let num2 = "";
 
 function operate() {
-    if (operator == "+") return add(num1, num2);
-    else if (operator == "-") return subtract(num1, num2);
-    else if (operator == "*") return multiply(num1, num2);
-    else if ( operator == "÷") return divide(num1, num2);
+    // Convert strings to actual numbers here
+    const n1 = Number(num1);
+    const n2 = Number(num2);
+    if (operator == "+") return add(n1, n2);
+    else if (operator == "-") return subtract(n1, n2);
+    else if (operator == "×") return multiply(n1, n2);
+    else if ( operator == "÷") return divide(n1, n2);
 }
+// display user input and calculation result
+const displayResult = document.querySelector('p')
+//user input operator 
+const inputOperator = document.querySelectorAll(".operator")
+inputOperator.forEach(operatorBtns => {
+    operatorBtns.addEventListener("click", function(e) {
+        operator = e.target.textContent;
+        displayResult.textContent = `${num1}${operator}${num2}`
+    })
+})
+// user input number update
+const inputNumber = document.querySelectorAll(".number")
+inputNumber.forEach(numBtns => {
+    numBtns.addEventListener("click", function(e) {
+        if (operator == "") {
+            num1 += e.target.textContent;
+
+        }else {
+            num2 += e.target.textContent;
+       }
+         displayResult.textContent = `${num1}${operator}${num2}`
+    })
+})
+// equalt to buttondisplayResult.textContent
+let result = "";
+const equalTo = document.querySelector(".brown")
+equalTo.addEventListener("click", () => {
+    result = operate();
+    displayResult.textContent = `${num1}${operator}${num2} = ${result}`
+    num1 = "";
+    num2 = "";
+    operator = "";
+}
+)
+// clear number
+const clearNumberBtn = document.querySelector(".clear")
+clearNumberBtn.addEventListener("click", () => {
+    displayResult.textContent = "";
+    num1 = "";
+    num2 = "";
+    operator = "";
+})
